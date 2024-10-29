45b33a8311ee789ff48ed7cfb403445041da8716
Ayse Karafil <ayseeekrnfl@gmail.com>
2024-10-29 23:34:55 +0000

    v4

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,6 @@ public class Runner {
         System.out.println("Version 1");
         System.out.println("Version 2");
         System.out.println("version 3");
+        System.out.println("version 4");
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,5 +2,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("version 3");
         System.out.println("versiom 4");
+        System.out.println("versiom 5");
     }
 }
